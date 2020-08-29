fdc76d974e26df0291568d9b46bc032ba3bc4ff1
co1ncidence <r3yan.chaudhry@zohomail.com>
2020-08-29 10:35:10 -0500

    rebuilding site Sat 29 Aug 2020 10:35:10 AM CDT

--- mysite/deploy.sh
+++ mysite/deploy.sh
@@ -6,20 +6,20 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo
 
-# Go To Public folder
+# Go To Website Folder
 cd ..
 
-# Add changes to git.
+# Stage everything
 git add .
 
-# Commit changes.
+# Commit 
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+# Push to master
 git push origin master
